8e244836ecff4f2673fd3a449e767f38ee7cb0eb
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-08-15 14:21:16 +0100

    integerPartition

--- integerPartition/script.js
+++ integerPartition/script.js
@@ -1,10 +1,17 @@
-// In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, is a way of writing n as a sum of positive integers.
-// Two sums that differ only in the order of their summands are considered the same partition. For example, 4 can be partitioned in five distinct ways:
-// 4
-// 3 + 1
-// 2 + 2
-// 2 + 1 + 1
-// 1 + 1 + 1 + 1
+/*
+
+    --- WORK IN PROGRESS ---
+
+==> In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, is a way of writing n as a sum of positive integers.
+Two sums that differ only in the order of their summands are considered the same partition. For example, 4 can be partitioned in five distinct ways:
+4
+3 + 1
+2 + 2
+2 + 1 + 1
+1 + 1 + 1 + 1
+Params: Integer.
+Output: All possible partitions.
+*/
 
 function createPartition(num) {
   const partitions = [num];
